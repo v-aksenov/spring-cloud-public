258a536274bc7214733934b415fc7641593fd142
laimo <laimonad@gmail.com>
2022-02-01 19:01:45 +0300

    Initial commit

---
+++ src/main/kotlin/me/aksenov/springcloudpublic/SpringCloudPublicApplication.kt
@@ -0,0 +1,11 @@
+package me.aksenov.springcloudpublic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudPublicApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCloudPublicApplication>(*args)
+}
